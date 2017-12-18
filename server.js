39417d755380ce1586ae8f44138d5000ebfbcd57
JC Jimenez <jcjimenez@gmail.com>
2017-12-18 15:50:59 -0600

    Basic project operations

--- server.js
+++ server.js
@@ -14,53 +14,35 @@ const projectController = require('./src/project');
 const blobService = azure.createBlobService();
 const queueService = azure.createQueueService();
 const tableService = azure.createTableService();
+const services = {
+  blobService:blobService,
+  queueService:queueService,
+  tableService:tableService  
+};
 
-const imageContainerName = process.env.IMAGE_CONTAINER_NAME || 'images';
-const imageQueueName = process.env.IMAGE_CONTAINER_NAME || 'images';
-const imageTableName = process.env.IMAGE_TABLE_NAME || 'images';
+collaborationController.setConfiguration(services);
+projectController.setConfiguration(services);
 
-async.series(
-  [
-    (callback) => { blobService.createContainerIfNotExists(imageContainerName, { publicAccessLevel: 'blob' }, callback); },
-    (callback) => { queueService.createQueueIfNotExists(imageQueueName, callback); },
-    (callback) => { tableService.createTableIfNotExists(imageTableName, callback); }
-  ],
-  (err, results) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+const schemaFile = fs.readFileSync("src/schema.graphql", "utf8");
 
-    const services = {
-      blobService:blobService,
-      queueService:queueService,
-      tableService:tableService  
-    };
+const collaborationSchemaFile = fs.readFileSync("src/collaboration.graphql", "utf8");
+const collaborationSchema = graphiql.buildSchema(schemaFile + collaborationSchemaFile);
 
-    collaborationController.setConfiguration(services);
-    projectController.setConfiguration(services);
-  
-    const schemaFile = fs.readFileSync("src/schema.graphql", "utf8");
+const projectSchemaFile = fs.readFileSync("src/project.graphql", "utf8");
+const projectSchema = graphiql.buildSchema(schemaFile + projectSchemaFile);
 
-    const collaborationSchemaFile = fs.readFileSync("src/collaboration.graphql", "utf8");
-    const collaborationSchema = graphiql.buildSchema(schemaFile + collaborationSchemaFile);
-
-    const projectSchemaFile = fs.readFileSync("src/project.graphql", "utf8");
-    const projectSchema = graphiql.buildSchema(schemaFile + projectSchemaFile);
-
-    const graphiqlEnabled = process.env.GRAPHIQL_ENABLED == 'true';
-    const app = express();
-    app.use(helmet());
-    app.use(express.static('web'))
-    app.use('/v1/graphql/collaboration', graphqlHTTP({
-      schema: collaborationSchema,
-      rootValue: collaborationController,
-      graphiql: graphiqlEnabled,
-    }));
-    app.use('/v1/graphql/project', graphqlHTTP({
-      schema: projectSchema,
-      rootValue: projectController,
-      graphiql: graphiqlEnabled,
-    }));
-    app.listen(process.env.PORT, () => console.log(`Started on port ${process.env.PORT}`))  
-});
+const graphiqlEnabled = process.env.GRAPHIQL_ENABLED == 'true';
+const app = express();
+app.use(helmet());
+app.use(express.static('web'))
+app.use('/v1/graphql/collaboration', graphqlHTTP({
+  schema: collaborationSchema,
+  rootValue: collaborationController,
+  graphiql: graphiqlEnabled,
+}));
+app.use('/v1/graphql/project', graphqlHTTP({
+  schema: projectSchema,
+  rootValue: projectController,
+  graphiql: graphiqlEnabled,
+}));
+app.listen(process.env.PORT, () => console.log(`Started on port ${process.env.PORT}`));
--- src/project.js
+++ src/project.js
@@ -2,6 +2,7 @@
 
 const Promise = require('promise');
 const uuid = require('uuid/v4');
+const async = require("async");
 
 const configuration = {
     blobService:null,
@@ -9,22 +10,64 @@ const configuration = {
     tableService:null
 };
 
+const imageContainerName = process.env.IMAGE_CONTAINER_NAME || 'images';
+const imageQueueName = process.env.IMAGE_CONTAINER_NAME || 'images';
+const imageTableName = process.env.IMAGE_TABLE_NAME || 'images';
+const projectTableName = process.env.IMAGE_TABLE_NAME || 'projects';
+
 module.exports = {
     setConfiguration:(configValues)=>{
         for(var k in configValues) configuration[k]=configValues[k];
+        async.series(
+            [
+              (callback) => { configuration.blobService.createContainerIfNotExists(imageContainerName, { publicAccessLevel: 'blob' }, callback); },
+              (callback) => { configuration.queueService.createQueueIfNotExists(imageQueueName, callback); },
+              (callback) => { configuration.tableService.createTableIfNotExists(imageTableName, callback); },
+              (callback) => { configuration.tableService.createTableIfNotExists(projectTableName, callback); }
+            ],
+            (err, results) => {
+                console.log("Project configuration set successfully.");
+            }
+        );
     },
     getProjects:(args, res)=>{
         return new Promise((resolve, reject)=>{
             // TODO: Ensure user has project access to the app.
-            resolve([
-                { projectId:uuid(), name:"Some Project" }
-            ]);
+            // var query = new azure.TableQuery().top(256);
+            configuration.tableService.queryEntities(projectTableName, null, null, (error, results, response)=>{
+                if (error) {
+                    reject(error);
+                    return;
+                }
+                console.log(results);
+                resolve(results.entries.map((value)=>{
+                    return {
+                        projectId:value.projectId._,
+                        name:value.name._
+                    }
+                }));
+            });
         });
     },
     createProject:(args, res)=>{
         return new Promise((resolve, reject)=>{
             // TODO: Ensure user has project access to the app.
-            resolve({ projectId:uuid(), name:"Some Project" });
+            const name = args.name;
+            const projectId = uuid().toString();
+            const project = {
+                PartitionKey: projectId,
+                RowKey: projectId,
+                projectId: projectId,
+                name: name
+            };
+            configuration.tableService.insertEntity(projectTableName, project, (error, result, response) => {
+                if (!error) {
+                    resolve(project);
+                }
+                else {
+                    reject(error);
+                }
+            });
         });
     },
     removeProject:(args, res)=>{
@@ -32,7 +75,17 @@ module.exports = {
             // TODO: Ensure projectId exists.
             // TODO: Ensure user has access to projectId.
             const projectId = args.projectId;
-            resolve({ projectId:projectId, name:"Some Project" });
+            configuration.tableService.deleteEntity(
+                projectTableName,
+                {PartitionKey:{"_":projectId}, RowKey:{"_":projectId}},
+                (error, response)=>{
+                    if (error) {
+                        reject(error);
+                        return;
+                    }
+                    resolve({ projectId:projectId, name:"Some Project" });
+                }
+            );
         });
     },
     allocateImages:(args, res)=>{
