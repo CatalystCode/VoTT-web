87a2fdc912817fd50b7ac9600a22e9dd3629cd33
JC Jimenez <jcjimenez@gmail.com>
2017-12-19 15:26:04 -0600

    Basic implementation of getNextImageTagTask

--- src/collaboration.js
+++ src/collaboration.js
@@ -14,20 +14,36 @@ module.exports = {
     },
     getNextImageTagTask:(args, res)=>{
         return new Promise((resolve, reject)=>{
-            // TODO: Ensure projectId exists.
-            // TODO: Ensure user has access to projectId.
             const projectId = args.projectId;
-            const simulatedDeployment = uuid();
-            resolve({
-                type:"ANNOTATE",
-                taskId:uuid(),
-                imageURL:`http://vott-${simulatedDeployment}.azure.com/images/${uuid()}.jpg`,
-                objectClassNames:["tide-detergent"],
-                instructions:{
-                    text:"Please draw a rectangle around any Tide detergent boxes you see.",
-                    sampleImageURL:`http://vott-${simulatedDeployment}.azure.com/images/sample.${uuid()}.jpg`,
-                    sampleVideoURL:`http://vott-${simulatedDeployment}.azure.com/images/sample.${uuid()}.m4v`
+
+            // TODO: Ensure user has access to projectId.
+
+            configuration.tableService.retrieveEntity("projects", "projects", projectId, (error, project)=>{
+                if (error) {
+                    return reject(error);
                 }
+                
+                console.log(project);
+                configuration.queueService.getMessage(projectId, (error, message)=>{
+                    if (error) {
+                        return reject(error);
+                    }
+
+                    const messageId = message.messageId;
+                    const messageText = message.messageText;
+                    const imageURL = configuration.blobService.getUrl(projectId, messageText);
+                    resolve({
+                        type: "ANNOTATE",
+                        taskId: messageId,
+                        imageURL: imageURL,
+                        objectClassNames: ["tide-detergent"],
+                        instructions:{
+                            text: "Please draw a rectangle around any Tide detergent boxes you see.",
+                            sampleImageURL: imageURL,
+                            sampleVideoURL: imageURL
+                        }
+                    });
+                });
             });
         });
     },
