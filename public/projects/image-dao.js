052e0370aa61db88deaeacf151824554c58580ce
jcjimenez <jc.jimenez@microsoft.com>
2018-01-23 11:19:49 -0600

    Factored out project and image data access objects.

--- public/projects/image-dao.js
+++ public/projects/image-dao.js
@@ -1,59 +1,26 @@
-const projectsGraphqlBasePath = '/v1/graphql/projects';
-
-function getProjects(nextPageToken) {
-  const query = nextPageToken ? `projects(nextPageToken:${JSON.stringify(nextPageToken)})` : 'projects';
-  return $.post(
-    projectsGraphqlBasePath,
-    { query: "query { "+query+"{ nextPageToken entries { projectId name taskType objectClassNames instructionsText } } }" }
-  );
+function ImageDAO () {
+  this.projectsGraphqlBasePath = '/v1/graphql/projects';
 }
 
-/**
- * Calls createProject(name: String!, taskType: TaskType!, objectClassNames:[String]!, instructionsText:String, instructionsImageURL:String, instructionsVideoURL:String):Project
- * @param {object} project whose properties include the values passed to the createProject function.
- */
-function createProject(project) {
-  const parameters = [
-    `name:${JSON.stringify(project.name)}`,
-    `taskType:${project.taskType}`,
-    `objectClassNames:${JSON.stringify(project.objectClassNames)}`,
-    `instructionsText:${JSON.stringify(project.instructionsText)}`
-  ].join(', ');
-  const query = `mutation { createProject (${parameters}) { projectId } }`;
-  console.log("Sending mutation query:");
-  console.log(query);
+ImageDAO.prototype.getImages = function (projectId) {
   return $.post(
-    projectsGraphqlBasePath,
-    { query: query }
-  );
-}
-
-function getImages(projectId) {
-  return $.post(
-    projectsGraphqlBasePath,
+    this.projectsGraphqlBasePath,
     {
       query: 'query { images(projectId: '+JSON.stringify(projectId)+'){ nextPageToken images {imageId imageURL} } }'
     }
   );
 }
 
-function createImages(projectId, files, callback) {
-  $.post(
-    projectsGraphqlBasePath,
+ImageDAO.prototype.createImages = function (projectId, files, callback) {
+  return $.post(
+    this.projectsGraphqlBasePath,
     {
       query: 'mutation { createImages(projectId: ' + JSON.stringify(projectId) + ', imageCount: ' + JSON.stringify(files.length) + ') { projectId imageId imageURL } }'
     }
-  ).done(function (result) {
-    if (result.errors) {
-      return callback(result.errors, null);
-    }
-    callback(null, result.data.createImages);
-  }).fail(function (error) {
-    callback(error);
-  });
+  );
 }
 
-function commitImages(images, callback) {
+ImageDAO.prototype.commitImages = function (images, callback) {
   // {
   //   query: 'mutation CommitImages($images: [ConfirmedImage]!) { commitImages(images: $images) }',
   //   variables: {
@@ -65,19 +32,12 @@ function commitImages(images, callback) {
   var escapedImages = images.map(function (value) {
     return '{ projectId: ' + JSON.stringify(value.projectId) + ', imageId: ' + JSON.stringify(value.imageId) + ' } ';
   });
-  $.post(
-    projectsGraphqlBasePath,
+  return $.post(
+    this.projectsGraphqlBasePath,
     {
       query: 'mutation { commitImages(images: [' + escapedImages.join() + '] ) }'
     }
-  ).done(function (result) {
-    if (result.errors) {
-      return callback(result.errors[0]);
-    }
-    callback(null, result.data.commitImages);
-  }).fail(function (error) {
-    callback(error);
-  });
+  );
 }
 
 // TODO: Support progress updates.
