2ff0050c706c0d154383b6c23103b71192ad1d21
hxlnt <partytimehexcellent@gmail.com>
2018-03-26 15:35:23 -0500

    Point to azurePostgresqlServer

--- config/env/production.js
+++ config/env/production.js
@@ -17,22 +17,22 @@ module.exports = {
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
 
-  // models: {
-  //   connection: 'someMysqlServer'
-  // },
+  models: {
+    connection: 'azurePostgresqlServer'
+  },
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  // port: 80,
+  port: 80,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
    ***************************************************************************/
 
-  // log: {
-  //   level: "silent"
-  // }
+  log: {
+    level: "silent"
+  }
 
 };
